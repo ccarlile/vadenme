663f1518ed60a4b6cfba7804f0fdda1d7873f358
Chris Carlile <christopher.carlile@gmail.com>
2016-02-23 15:44:54 -0600

    Live stuffs

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -48,16 +46,16 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
+//if (app.get('env') === 'development') {
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err,
+//            title: 'error'
+//        });
+//    });
+//}
 
 // production error handler
 // no stacktraces leaked to user
